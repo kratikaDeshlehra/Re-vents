85c73e4f16fefc40c82546c0d08dfa0cc3f64c6c
kratika <kratikadeshlehra@gmail.com>
2025-01-18 23:20:42 +0530

    created a signedInMenu

--- src/app/Layout/nav/NavBar.tsx
+++ src/app/Layout/nav/NavBar.tsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import { Button, Container, Menu, MenuItem } from "semantic-ui-react";
+import SignedInMenu from "./SignedInMenu";
 
 export default function NavBar(){
     return (
@@ -16,11 +17,7 @@ export default function NavBar(){
                         as ={NavLink} to='/createEvent'
                         floated='right' positive={true} inverted={true} content='Create Event' />
                     </MenuItem>
-                    <MenuItem position="right">
-                        <Button basic inverted content='Login' />
-                        <Button basic inverted content='Register' style={{ marginLeft: '0.5em' }} />
-
-                    </MenuItem>
+                    <SignedInMenu/>
                 </Container>
             </Menu>
         </div>
