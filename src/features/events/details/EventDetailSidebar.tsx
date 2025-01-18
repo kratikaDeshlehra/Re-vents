a770cec3683eb0b5892ab3ff4cac01ebfe0a9ded
kratika <kratikadeshlehra@gmail.com>
2025-01-19 01:14:55 +0530

    add components in event detail section

--- src/features/events/details/EventDetailSidebar.tsx
+++ src/features/events/details/EventDetailSidebar.tsx
@@ -1,9 +1,40 @@
+import { Segment, Item } from "semantic-ui-react";
 
 
 export default function EventDetailSidebar() {
   return (
-    <div>
-      
-    </div>
+  
+<>
+<Segment
+    textAlign="center"
+    style={{border: 'none'}}
+    attached="top"
+    secondary
+    inverted
+    color="teal"
+>
+    2 People Going
+</Segment>
+<Segment attached>
+    <Item.Group relaxed divided>
+        <Item style={{position: 'relative'}}>
+            <Item.Image size="tiny" src='http://localhost:3000/categoryImages/user.png'/>
+            <Item.Content verticalAlign="middle">
+                <Item.Header as="h3">
+                    <span>Tom</span>
+                </Item.Header>
+            </Item.Content>
+        </Item>
+        <Item style={{position: 'relative'}}>
+            <Item.Image size="tiny" src='http://localhost:3000/categoryImages/user.png'/>
+            <Item.Content verticalAlign="middle">
+                <Item.Header as="h3">
+                    <span>Bob</span>
+                </Item.Header>
+            </Item.Content>
+        </Item>
+    </Item.Group>
+</Segment>
+</>
   )
 }
