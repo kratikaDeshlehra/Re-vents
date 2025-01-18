0b373cb5eafa3ec3e4d0c5e20993a7a34fa3f9b7
kratika <kratikadeshlehra@gmail.com>
2025-01-18 23:51:21 +0530

    fake authentication created

--- src/app/Layout/nav/NavBar.tsx
+++ src/app/Layout/nav/NavBar.tsx
@@ -1,8 +1,12 @@
 import { NavLink } from "react-router-dom";
 import { Button, Container, Menu, MenuItem } from "semantic-ui-react";
 import SignedInMenu from "./SignedInMenu";
+import { useState } from "react";
+import SignedOutButton from "./SignedOutButton";
 
 export default function NavBar(){
+
+   const [auth,setAuth]=useState(false);
     return (
         <div>
             <Menu inverted={true} fixed='top'>
@@ -17,7 +21,8 @@ export default function NavBar(){
                         as ={NavLink} to='/createEvent'
                         floated='right' positive={true} inverted={true} content='Create Event' />
                     </MenuItem>
-                    <SignedInMenu/>
+                    {auth ? <SignedInMenu setAuth={setAuth}/>:<SignedOutButton setAuth={setAuth}/>}
+                    
                 </Container>
             </Menu>
         </div>
--- src/app/Layout/nav/SignedOutButton.tsx
+++ src/app/Layout/nav/SignedOutButton.tsx
@@ -1,10 +1,14 @@
 import { Button, MenuItem } from "semantic-ui-react";
 
-
-export default function SignedOutButton() {
+type Props={
+    setAuth:(value: boolean )=> void;
+}
+export default function SignedOutButton({setAuth}:Props) {
   return (
     <MenuItem position="right">
-    <Button basic inverted content='Login' />
+    <Button basic inverted content='Login' onClick={()=>{
+        setAuth(true);
+    }} />
     <Button basic inverted content='Register' style={{ marginLeft: '0.5em' }} />
 
 </MenuItem>
