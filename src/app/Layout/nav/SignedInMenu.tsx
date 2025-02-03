39b4591fc3a7d15529e6ae39c33aef1045eb978a
kratika <kratikadeshlehra@gmail.com>
2025-02-03 22:36:41 +0530

    added accounts page

--- src/app/Layout/nav/SignedInMenu.tsx
+++ src/app/Layout/nav/SignedInMenu.tsx
@@ -21,6 +21,7 @@ export default function SignedInMenu() {
                 <Dropdown.Menu>
                     <Dropdown.Item as={Link} to='/createEvent' text='Create event' icon='plus' />
                     <Dropdown.Item text='My profile' icon='user' />
+                    <Dropdown.Item  as={Link} to='/account' text='My Account' icon='settings' />
                     <Dropdown.Item onClick={handleSignOut}text='Sign out' icon='power'  />
                 </Dropdown.Menu>
 
--- src/app/router/Routes.tsx
+++ src/app/router/Routes.tsx
@@ -3,6 +3,7 @@ import App from "../Layout/App";
 import EventDashboard from "../../features/events/dashboard/EventDashboard";
 import EventDetailedPage from "../../features/events/details/EventDetailedPage";
 import EventForm from "../../features/events/form/EventForm";
+import AccountPage from "../../features/auth/AccountPage";
 
 export const router=createBrowserRouter([
     {
@@ -12,7 +13,8 @@ export const router=createBrowserRouter([
             {path : '/events',element:<EventDashboard/>},
             {path : '/events/:id',element:<EventDetailedPage/>},
             {path : '/manage/:id',element:<EventForm/>},
-            {path : '/createEvent',element:<EventForm key='create'/>}
+            {path : '/createEvent',element:<EventForm key='create'/>},
+            {path : '/account',element:<AccountPage/>}
 
         ]
 
