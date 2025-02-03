43a3e7c5cbfbded3b254dd1b241075c2d5d6e786
kratika <kratikadeshlehra@gmail.com>
2025-02-03 23:01:20 +0530

    added custom validation

--- src/features/auth/AccountPage.tsx
+++ src/features/auth/AccountPage.tsx
@@ -2,12 +2,21 @@ import { FieldValues, useForm } from "react-hook-form"
 import { Link } from "react-router-dom";
 import { Button, Form, Header, Icon, Segment } from "semantic-ui-react";
 import { useAppSelector } from "../../app/store/store";
+import { useEffect } from "react";
 export default function AccountPage() {
     const {currentUser}=useAppSelector(state => state.auth);
-        const { register, handleSubmit, formState: { errors, isSubmitting, isValid } } = useForm({
+        const { register, handleSubmit,getValues, setValue,watch,trigger,formState: { errors, isSubmitting, isValid } } = useForm({
         mode: 'onTouched'
     })
+    
+    const password1=watch('password1');
+    const password2=watch('password2');
 
+    useEffect (()=>{
+         
+        if(password2) trigger('password2');
+
+    },[password2,trigger,password1])
     function onSubmit(data: FieldValues) {
         console.log(data);
     }
@@ -23,16 +32,31 @@ export default function AccountPage() {
                         type='password'
                         defaultValue=''
                         placeholder='Password'
-                        {...register('password1', { required: true })}
-                        error={errors.password1 && 'Password is required '}
+                        {...register('password1', { required: 'Password is required',
+                          })}
+                        error={errors.password1 && errors.password1.message} 
+                        onChange={(e) => {
+
+                            setValue('password1', e.target.value, { shouldValidate: true });
+                        }}
                     />
 
                     <Form.Input
                         type='password'
                         defaultValue=''
                         placeholder='Confirm Password'
-                        {...register('password2', { required: true })}
-                        error={errors.password2 && 'Confirm Password is required '}
+                        {...register('password2', { required: 'Confirm password is required',
+                            validate:{
+                                passwordMatch: value=> (value===getValues().password1) || 'Password do not match'
+                           } 
+                         })}
+                        error={errors.password2?.type==='required' && errors.password2.message || 
+                            errors.password2?.type==='passwordMatch' && errors.password2.message
+                        } 
+                        onChange={(e) => {
+
+                            setValue('password2', e.target.value, { shouldValidate: true });
+                        }}
                     /> 
 
                     <Button loading={isSubmitting} 
