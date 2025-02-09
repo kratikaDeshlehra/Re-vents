cdf40a4117ff71a3f99797de4cf8e13d68ef9075
kratika <kratikadeshlehra@gmail.com>
2025-02-09 14:41:28 +0530

    added firebase database to the project

--- src/app/config/Firebase.ts
+++ src/app/config/Firebase.ts
@@ -2,12 +2,15 @@
 import { initializeApp } from "firebase/app";
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/database'
 import {  getFirestore } from "firebase/firestore";
 import { getAuth} from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  databaseURL: "https://revents-585b6-default-rtdb.asia-southeast1.firebasedatabase.app",
   authDomain: "revents-585b6.firebaseapp.com",
   projectId: "revents-585b6",
   storageBucket: "revents-585b6.firebasestorage.app",
@@ -22,5 +25,5 @@ export const db=getFirestore(app);
 
 export const auth=getAuth(app);
 
-
+export const fb=getDatabase(app);
 
